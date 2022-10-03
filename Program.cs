a37bc1683cb8aa08c0c22264b59e4029c2867bea
olivergirard <girard1@purdue.edu>
2022-10-03 10:08:51 -0400

    worked on hiragana conversion method

--- Program.cs
+++ Program.cs
@@ -8,6 +8,16 @@ namespace VCVLyricInserter
 {
     class Program
     {
+        static string[] A = { "あ", "か", "さ", "た", "な", "は", "ま", "や", "ら", "わ", "が", "ざ", "だ", "ば", "ぱ", "きゃ", "ぎゃ", "にゃ", "ひゃ", "びゃ", "ぴゃ", "みゃ", "りゃ", "じゃ", "ちゃ", "しゃ" };
+
+        static string[] I = {"い", "き", "し", "ち", "に", "ひ", "み", "り", "ぎ", "じ", "び", "ぴ" };
+
+        static string[] U = { "う", "く", "す", "つ", "ぬ", "ふ", "む", "ゆ", "る", "ぐ", "ず", "ぶ", "ぷ", "きゅ", "ぎゅ", "にゅ", "ひゅ", "びゅ", "ぴゅ", "みゅ", "りゅ", "じゅ", "ちゅ", "しゅ" };
+
+        static string[] E = { "え", "け", "せ", "て", "ね", "へ", "め", "れ", "げ", "ぜ", "で", "べ", "ぺ", "じぇ", "ちぇ", "しぇ" };
+
+        static string[] O = { "お", "こ", "そ", "と", "の", "ほ", "も", "よ", "ろ", "を", "ご", "ぞ", "ど", "ぼ", "ぽ", "きょ", "ぎょ", "にょ", "ひょ", "びょ", "ぴょ", "みょ", "りょ", "ちょ", "じょ", "しょ" };
+
         static void Main(string[] args)
         {
             EncodingProvider provider = CodePagesEncodingProvider.Instance;
@@ -26,19 +36,19 @@ namespace VCVLyricInserter
         {
 
             Console.WriteLine("Enter your VCV characters in the following format:\n");
-            Console.WriteLine("<romaji vowel><hiragana/romaji character>_<romaji vowel><hiragana/romaji character>");
+            Console.WriteLine("<hiragana/romaji character> <hiragana/romaji character>");
 
             string characters = Console.ReadLine();
 
-            /* removes underscores */
+            /* removes spaces */
 
-           if (characters.Contains('_') == false)
+           if (characters.Contains(' ') == false)
            {
                Console.WriteLine("It seems there was an error. Please make sure your lyrics are formatted correctly and try again.");
                VCV(utauPlugin);
            }
 
-            char firstSound = char.Parse(characters.Substring(2, 3));
+            char firstSound = char.Parse(characters.Substring(0, 1));
 
             if (((firstSound >= 65) && (firstSound <= 90)) || ((firstSound <= 97) && (firstSound <= 122)))
             {
@@ -62,19 +72,20 @@ namespace VCVLyricInserter
 
             //TODO exception handling during debug testing
 
-            string lyric = "";
             int index = 0;
+            string lyric = "";
+            string previousPhoneme = "";
 
-            while (characters.Contains('_'))
+            while (characters.Contains(' '))
             {
-                lyric += characters.Substring(0, 1);
-                lyric += " ";
-                characters = characters.Substring(1);
-                
-                if ((characters.Substring(0, 2)).Contains('_'))
+                if (characters.Substring(0, 2).Contains(' '))
                 {
                     /* short vowels */
+                    
+                    lyric += DetermineVowel(previousPhoneme);
+                    previousPhoneme = characters.Substring(0, 1);
 
+                    lyric += " ";
                     lyric += characters.Substring(0, 1);
                     characters = characters.Substring(2);
 
@@ -82,9 +93,14 @@ namespace VCVLyricInserter
                 {
                     /* long vowels */
 
+                    lyric += DetermineVowel(previousPhoneme);
+                    previousPhoneme = characters.Substring(0, 2);
+
+                    lyric += " ";
                     lyric += characters.Substring(0, 2);
                     characters = characters.Substring(3);
                 }
+
                 Note note = utauPlugin.note[index];
 
                 note.SetLyric(lyric);
@@ -92,6 +108,55 @@ namespace VCVLyricInserter
                 lyric = "";
                 index++;
             }
+        }
+
+        static string DetermineVowel(string previousPhoneme)
+        {
+            if (previousPhoneme == "") {
+                return "-";
+            }
+
+            foreach(string phoneme in A) {
+                if (previousPhoneme == phoneme)
+                {
+                    return "a";
+                }
+            }
+
+            foreach(string phoneme in I)
+            {
+                if (previousPhoneme == phoneme)
+                {
+                    return "i";
+                }
+            }
+
+            foreach (string phoneme in U)
+            {
+                if (previousPhoneme == phoneme)
+                {
+                    return "u";
+                }
+            }
+
+            foreach (string phoneme in E)
+            {
+                if (previousPhoneme == phoneme)
+                {
+                    return "e";
+                }
+            }
+            
+            foreach (string phoneme in O)
+            {
+                if (previousPhoneme == phoneme)
+                {
+                    return "o";
+                }
+            }
+
+            return "n";
+
         }
             
     }
